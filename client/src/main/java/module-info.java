2b488d30d14a84b4025c4eb6240303e12857c200
ankit219302-git <ankit219302@gmail.com>
2025-12-05 02:27:30 +0530

    cleaned up pom and module-info

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,5 +1,4 @@
 module cryptography.client {
-    requires org.apache.commons.codec;
     requires cryptography.encryption;
     requires cryptography.decryption;
     requires org.apache.commons.lang3;
--- encryption/src/main/java/module-info.java
+++ encryption/src/main/java/module-info.java
@@ -1,6 +1,4 @@
 module cryptography.encryption {
-    requires org.apache.commons.codec;
-    requires org.apache.commons.lang3;
     exports com.cybsec.cryptography.encryption to cryptography.client;
     exports com.cybsec.cryptography.encryption.impl to cryptography.client;
 }
