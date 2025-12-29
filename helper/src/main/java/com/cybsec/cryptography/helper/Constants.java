7f2ded872acaa1368b4cc80b736cacfeb06b73d4
ankit219302-git <ankit219302@gmail.com>
2025-12-30 02:20:54 +0530

    updated securerandom to use cryptographically strong instance

--- helper/src/main/java/com/cybsec/cryptography/helper/Constants.java
+++ helper/src/main/java/com/cybsec/cryptography/helper/Constants.java
@@ -1,9 +1,10 @@
 package com.cybsec.cryptography.helper;
 
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public final class Constants {
-    public static final SecureRandom SECURE_RANDOM = new SecureRandom();
+    public static final SecureRandom SECURE_RANDOM;
     public static final String DEFAULT_ASYMMETRIC_CRYPTOGRAPHY = "RSA";
     public static final String DEFAULT_SYMMETRIC_CRYPTOGRAPHY = "AES";
     public static final String RSA_OAEP_SHA256_MGF1_ALGORITHM = "RSA/ECB/OAEPWithSHA-256AndMGF1Padding";
@@ -15,6 +16,13 @@ public final class Constants {
     public static final int AES_GCM_IV_LENGTH_BYTES = 12; // 96 bits, recommended
     public static final int AES_GCM_AUTH_TAG_LENGTH_BITS = 128; // 128-bit authentication tag
     public static final int AES_CBC_IV_LENGTH_BYTES = 16; // 128 bits
+    static {
+        try {
+            SECURE_RANDOM = SecureRandom.getInstanceStrong();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private Constants() {}
 }
